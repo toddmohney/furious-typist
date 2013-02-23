039f17c08d1f286f9d2b36a7287dfd8fa904fba4
toddmohney <toddmohney@gmail.com>
2013-02-23 11:44:46 -0500

    Add cuke helper: generic url identifier

--- features/step_definitions/helper_steps.rb
+++ features/step_definitions/helper_steps.rb
@@ -13,6 +13,22 @@ When /^I go to my "(.*?)" page$/ do |page|
   visit path_to(page)
 end
 
+Then /^I should be on the "([^"]*)"( [\w]*)? page$/ do |page_name, format|
+  start = Time.now
+  while true
+    current_path = URI.parse(current_url).select(:path, :query).compact.join('?')
+    expected_path = path_to(page_name)
+    if format.present?
+      expected_path = expected_path << ".#{format.gsub(" ", "")}"
+    end
+    break if current_path == expected_path
+    if Time.now > start + 1.seconds
+      fail "Expected to be at #{expected_path}, but actually at #{current_path}"
+    end
+    sleep 0.1
+  end
+end
+
 module StepHelpers
   def current_path
     URI.parse(current_url).path
