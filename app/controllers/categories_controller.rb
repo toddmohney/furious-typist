1a469decc90f4f06d931ec31ea33632602956bd7
toddmohney <toddmohney@gmail.com>
2013-03-02 18:56:40 -0500

    Clean up genereated responders in all controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,73 +2,39 @@ class CategoriesController < ApplicationController
 
   before_filter :authenticate_user!
 
-  # GET /categories
-  # GET /categories.json
   def index
     @categories = Category.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @categories }
-    end
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
   def show
     @category = Category.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @category }
-    end
   end
 
-  # GET /categories/new
-  # GET /categories/new.json
   def new
     @category = Category.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @category }
-    end
   end
 
-  # GET /categories/1/edit
   def edit
     @category = Category.find(params[:id])
   end
 
-  # POST /categories
-  # POST /categories.json
   def create
     @category = Category.new(params[:category])
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to @category, :notice => 'Category was successfully created.' }
-        format.json { render :json => @category, :status => :created, :location => @category }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @category.errors, :status => :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to @category, :notice => 'Category was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /categories/1
-  # PUT /categories/1.json
   def update
     @category = Category.find(params[:id])
 
-    respond_to do |format|
-      if @category.update_attributes(params[:category])
-        format.html { redirect_to @category, :notice => 'Category was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @category.errors, :status => :unprocessable_entity }
-      end
+    if @category.update_attributes(params[:category])
+      redirect_to @category, :notice => 'Category was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
@@ -78,9 +44,6 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @category.destroy
 
-    respond_to do |format|
-      format.html { redirect_to categories_url }
-      format.json { head :no_content }
-    end
+    redirect_to categories_url
   end
 end
