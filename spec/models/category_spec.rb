b4026cd72254663e443ae02e7945f0762714abc8
toddmohney <toddmohney@gmail.com>
2013-09-29 10:42:56 -0400

    Refactor Category specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,7 @@
 require 'spec_helper'
 
 describe Category do
-  describe "validation" do
-    it "fails validation without a name" do
-      expect(Category.new).to have(1).error_on(:name)
-    end
-
-    it "passes validation with a name and description" do
-      expect(Category.new({ :name => "test name",
-                            :description => "test description" })).to have(0).errors
-    end
-  end
-
-  describe "factories" do
-    it "has a valid factory" do
-      FactoryGirl.create(:category).should be_valid
-    end
+  describe "validations" do
+    it { should validate_presence_of(:name) }
   end
 end
