8c7b4f8c693ed2af7de7c0fef76629b1fabefae5
toddmohney <toddmohney@gmail.com>
2013-09-29 10:20:05 -0400

    Add tests for User after_initialize callbacks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,23 @@ describe User do
     it { should validate_presence_of(:username) }
     it { should ensure_length_of(:username).is_at_most(64) }
   end
+
+  describe "callbacks" do
+    describe "after_initialize" do
+      let(:user_role) { double(:user_role) }
+      let(:user_roles) { [] }
+
+      before do
+        Role.stub(:where).with({ name: "user" }) { user_role }
+        User.any_instance.stub(:roles) { user_roles }
+      end
+
+      it "adds the default 'user' role" do
+        user = User.new
+        user.roles.should == [ user_role ]
+      end
+    end
+  end
 end
 
 
